7893c75ee0c1c948d6c82af27f306a256b982386
Trevor Jones <tjones009@yahoo.com>
2020-02-02 18:36:08 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,7 +2,7 @@ def using_until
   levitation_force = 6
   #your code here
 
-until levitation_force == 16 do
+until levitation_force = 16 do
   puts "Wingardium Leviosa"
   levitation_force = levitation_force + 1
 
